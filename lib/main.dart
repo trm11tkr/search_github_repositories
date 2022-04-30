dccfabc188468369975f6d4093c34122e5eaa8ac
森田陸斗 <mrmrt1121@gmail.com>
2022-04-30 15:35:51 +0900

    Display of acquired data

--- lib/main.dart
+++ lib/main.dart
@@ -16,37 +16,51 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends ConsumerWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final inputText = ref.watch(apiProvider).asData?.value;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: const Text('Search Repositories'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
-              onChanged: (text){
-                try {
-                  ref.watch(inputTextProvider.state).state = text;
-                  print(text);
-                } catch (ex) {}
-              },
+              onChanged: (text) => onInputTextChanged(ref, text),
             ),
+            Flexible(
+              child: ListView.builder(
+                  itemCount: inputText?.items.length ?? 0,
+                  itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(inputText?.items[index].name ?? 'null'),
+                  );
+                  }
+              ),
+            ),
+
           ],
         ),
       ),
     );
+  }
+}
+
+void onInputTextChanged(WidgetRef ref, String text) {
+  try {
+    ref.watch(inputTextProvider.state).state = text;
+  } catch (ex) {
+    print(ex.toString());
   }
 }
